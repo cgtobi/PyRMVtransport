840724c9fb8bf5ad5dcf5077287f2f13f44961e9
Tobias Sauerwein <cgtobi@gmail.com>
2019-03-21 23:48:10 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyRMVtransport",
-    version="0.2.2",
+    version="0.2.3",
     author="cgtobi",
     author_email="cgtobi@gmail.com",
     python_requires=">=3.6.0",
