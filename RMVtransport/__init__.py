3877451b7685bf8a971e16806b5c2a7b268b76a1
Tobias Sauerwein <cgtobi@gmail.com>
2021-02-19 11:09:23 +0100

    Bump version

--- RMVtransport/__init__.py
+++ RMVtransport/__init__.py
@@ -4,5 +4,5 @@ from .rmvtransport import RMVtransport  # noqa
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 3
-PATCH_VERSION = 0
+PATCH_VERSION = 1
 __version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}.{PATCH_VERSION}"
